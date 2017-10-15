40e3f39de72e47478b71dc1afe54dc82b7e5fdd2
Danilo Raulino de Liz <danilo.rdliz@gmail.com>
2017-10-15 20:27:53 -0200

    chore: material-ui configuração e importação.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, compose } from 'redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import reducer from './reducers'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectTapEventPlugin();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -15,7 +19,9 @@ const store = createStore(
 )
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+    <MuiThemeProvider>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
